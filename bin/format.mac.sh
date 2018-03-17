2e87d9b80dd34a8544bcd3a59309c6b77e8adbe0
rolevax <rolevax@gmail.com>
2018-03-17 20:51:25 +0800

    update libsaki w/ lua

--- bin/format.mac.sh
+++ bin/format.mac.sh
@@ -2,7 +2,7 @@
 
 # format libsaki
 cd libsaki
-FILES=`git ls-files -mo --exclude-standard | grep "\.cpp\|\.h"`
+FILES=`git ls-files -mo --exclude-standard | grep "\.cpp\|\.h"` | grep -v "3rd"
 if [ ! -z "${FILES}" ]; then
   echo "Format:" ${FILES}
   ../bin/uncrustify.mac -c ../uncrustify.cfg --no-backup ${FILES}
