c0cf86d83fbabe6ac650da565f47a55c7d8d4f3c
rolevax <rolevax@gmail.com>
2019-03-10 00:33:23 +0800

    bump to 0.10.0

--- gui/p_global.cpp
+++ gui/p_global.cpp
@@ -107,7 +107,7 @@ bool PGlobal::official()
 QString PGlobal::version()
 {
 #ifdef PANCAKE_OFFICIAL
-    return QString("0.9.7");
+    return QString("0.10.0");
 #else
     return QString("custom-fork");
 #endif
